e577a0aea2245a125dc3bcfb76a15d87ec7e16fe
mahilmithranks <mahilmithranks2007@gmail.com>
2025-02-21 14:45:44 +0530

    done

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("Connected to the database"))
+.catch((err)=>console.log("Error Connecting to database",err));
+
+app.listen(PORT,()=>{
+    console.log(`Server running on ${PORT}`);
+});
